51945c0a6621e44964dd02c17178d4e9bb4c4c69
rifkyhilman <rifkyhilman265@gmail.com>
2020-01-23 22:32:59 +0700

    new add

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   import x from "@fortawesome/fontawesome-free/js/all";
-  import Navbar from "./components/navbar.svelte";
+  import Navbar from "./components/Navbar.svelte";
+  import Home from "./Home.svelte";
 </script>
 
 <Navbar />
+<Home />
---
+++ src/Home.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Title from "./components/Title.svelte";
+</script>
+
+<Title />
+<Title title="add Expense" />
+<Title title="Expense list" />
+<Title />
